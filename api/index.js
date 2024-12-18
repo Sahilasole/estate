c8ec861dd188d866190742174ad4484f70edb98c
Sahilasole <sahilasole2122@gmail.com>
2024-12-18 17:04:06 +0530

    create and run the server

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect("")
 
 const app= express();
 
 app.listen(4000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 4000!!! ');
     }
 );
